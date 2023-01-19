53f905115bf0b25b4fd0b59d1b2f34351186382c
jaken92 <jaaakeeen@gmail.com>
2023-01-19 21:41:27 +0100

    taking over project from Robin.
    Created a summary.php where im goiong to present statistics from logbook and db.

---
+++ summary.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Summary of all the bookings in logbook below.</h1>
+    <h2>Summary of the hotels revenue for January 2023, aswell as most popular feature and average revenue per booking.</h2>
+
+</body>
+
+</html>
