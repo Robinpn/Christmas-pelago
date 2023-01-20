df8095df74eaa3ac6d6d7884fc0bd5e83d2d87bc
jaken92 <jaaakeeen@gmail.com>
2023-01-20 18:14:50 +0100

    presenting logbook data

--- summary.php
+++ summary.php
@@ -1,6 +1,17 @@
 <?php
 
+$logbookData = json_decode(file_get_contents(__DIR__ . '/logbook.json'), true);
+
+
+// foreach ($logbookData['vacation'] as $stay) {
+//     echo $stay['island'];
+// }
+
+
 ?>
+
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,12 +20,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/summary.css">
     <title>Document</title>
 </head>
 
 <body>
-    <h1>Summary of all the bookings in logbook below.</h1>
-    <h2>Summary of the hotels revenue for January 2023, aswell as most popular feature and average revenue per booking.</h2>
+    <header>
+        <h1>
+            Christmas-Pelago
+        </h1>
+    </header>
+    <h2>Summary of all the bookings in logbook: </h1>
+        <?php foreach ($logbookData['vacation'] as $stay) : ?>
+            <div class="card-container">
+                <div class="text-container">
+                    <h3>Hotel: <?= $stay['hotel'] ?></h3>
+                    <h4>Island: <?= $stay['island'] ?></h4>
+                    <li>Stars: <?= $stay['stars'] ?></li>
+                    <li>Arrival date: <?= $stay['arrival_date'] ?></li>
+                    <li>Departure date: <?= $stay['departure_date'] ?></li>
+                    <li>
+                        Features:
+                        <?php foreach ($stay['features'] as $feature) : ?>
+                            <?= $feature['name'] . " "; ?>
+                        <?php endforeach ?>
+                    </li>
+                    <li>Total cost: <?= $stay['total_cost'] . "$"; ?></li>
+                </div>
+            </div>
+        <?php endforeach; ?>
+        <h2>Summary of the hotels revenue for January 2023, aswell as most popular feature and average revenue per booking:</h2>
 
 </body>
 
